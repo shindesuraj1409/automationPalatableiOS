d573df9d683f6cdb9dbe2b8ece4561d5c1abc49f
suraj <shindesuraj1409@gmail.com>
2020-05-28 00:32:24 +0530

    Get Statrted Clicked

--- src/test/java/automation/palatable/ios/TestCase_Sample001.java
+++ src/test/java/automation/palatable/ios/TestCase_Sample001.java
@@ -1,5 +1,6 @@
 package automation.palatable.ios;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
@@ -13,9 +14,11 @@ public class TestCase_Sample001 extends BaseClass
 	
 	
 	@Test
-	public void sampleTest()
+	public void sampleTest() throws InterruptedException
 	{
 		System.out.println("Nothing to Test");
+		Thread.sleep(3000);
+		driver.findElement(By.name("GET STARTED")).click();
 	}
 	
 
